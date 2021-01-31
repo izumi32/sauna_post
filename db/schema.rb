8b0ba8e445539ee7be451e8e37479f73fec69525
izumi32 <k.izumi721@gmail.com>
2021-01-31 18:32:08 +0900

    データベース上のemailの一意性

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_085238) do
+ActiveRecord::Schema.define(version: 2021_01_31_084112) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_01_30_085238) do
     t.datetime "activated_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,7 +31,6 @@ class UserTest < ActiveSupport::TestCase
 
   test "emailが重複している場合" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
