def087e0fbf21ebec4bd901ec330edf05f74bea1
izumi32 <k.izumi721@gmail.com>
2021-02-02 20:40:11 +0900

    無効なユーザーのログインテスト

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #@userを保存
+    else
+      flash.now[:danger] = "ログインに失敗しました"
+      render 'new'
+    end
   end
 
   def destroy
