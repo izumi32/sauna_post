b0875d2c423568b81708e9f6c9c9cebd9d926163
izumi32 <k.izumi721@gmail.com>
2021-02-03 17:56:19 +0900

    永続的セッションのテスト

---
+++ test/helpers/session_helper_test.rb
@@ -0,0 +1,17 @@
+class SessionsHelperTest < ActionView::TestCase
+
+  def setup
+    @user = users(:michael)
+    remember(@user)
+  end
+
+  test "sessionがnilでログイン" do
+    assert_equal @user, current_user
+    assert logged_in?
+  end
+
+  test "remember_digestに別のハッシュが入っている時のログイン" do
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,17 +18,28 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "有効なユーザーのログイン/ログアウト" do
     get login_path
-    post login_path, params: { session: { email: @user.email,
-                                          password: "password" } }
-    assert is_logged_in?
+    log_in @user
+    assert logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     delete logout_path
-    assert_not is_logged_in?
+    assert_not logged_in?
     assert_redirected_to root_url
     delete logout_path
+  end
+
+  test "永続的セッションを維持してログイン" do
+    log_in @user, remember_me: '1'
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "永続的セッションを維持せずログイン" do
+    log_in @user, remember_me: '1'
+    delete logout_path
+    log_in @user, remember_me: '0'
+    assert_empty cookies[:remember_token]
   end
 end
