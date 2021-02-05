f1f8d325caeda556041ce6b559fd8c3d2ca90377
izumi32 <k.izumi721@gmail.com>
2021-02-05 19:53:25 +0900

    投稿ページ作成

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,32 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user
+
+  def index
+    redirect_to newpost_path
+  end
+
+  def new
+    @micropost = current_user.microposts.build
+  end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "投稿を作成しました"
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @micropost = current_user.micropost.find_by(id: params[:id])
+    @micropost.destroy
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit(:name, :address, :price, :sauna, :evaluate)
+  end
 end
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @micropost = microposts(:gokuraku)
+  end
+
+  test "非ログイン時newにリクエスト" do
+    get newpost_path
+    assert_redirected_to login_url
+  end
+
+  test "非ログイン時createにリクエスト" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { microposts: { name: "久松湯",
+                                                    address: "東京都練馬区",
+                                                    price: 500,
+                                                    sauna: 1,
+                                                    evaluate: 3 } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "非ログイン時destroyにリクエスト" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+    assert_redirected_to login_url
+  end
 end
