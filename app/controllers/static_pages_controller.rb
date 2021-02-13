add76c647e30bba6b953b24c882b978a8cbe7bf4
izumi32 <k.izumi721@gmail.com>
2021-02-13 18:03:48 +0900

    投稿検索（名前と住所)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,14 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
+      @search_params = micropost_search_params
+      @feed_items = current_user.feed.paginate(page: params[:page]).search(@search_params)
     end
+  end
+
+  private
+
+  def micropost_search_params
+    params.fetch(:search, {}).permit(:name, :address)
   end
 end
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,18 +7,36 @@ class Micropost < ApplicationRecord
   validates :sauna, presence: true,
                     numericality: { greater_than_or_equal_to: 0,
                                     less_than_or_equal_to: 1,
-                                    only_integer: true}
+                                    only_integer: true }
   validates :evaluate, presence: true,
                        numericality: { greater_than_or_equal_to: 1,
                                        less_than_or_equal_to: 5,
                                        only_integer: true }
   validates :user_id, presence: true
 
-  def Micropost.search(search_word)
-    if search_word
-      self.where(['name LIKE ?', "%#{search_word}%"])
-    else
-      self.all
-    end
+  scope :search, -> (search_params) do
+
+    # search_paramsが空の場合以降の処理を行わない
+    return if search_params.blank?
+
+    name_like(search_params[:name])
+    .address_like(search_params[:address])
+
   end
+
+  scope :name_like, -> (name) {
+    where('name LIKE ?', "%#{name}%") if name.present?
+  }
+
+  scope :address_like, -> (address) {
+    where('address LIKE ?', "%#{address}%") if address.present?
+  }
+
+  # def Micropost.search(search_word)
+  #   if search_word
+  #     self.where(['name LIKE ?', "%#{search_word}%"])
+  #   else
+  #     self.all
+  #   end
+  # end
 end
