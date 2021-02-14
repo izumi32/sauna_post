fa01ba355a042277bcc02c43ab26912f4a093862
izumi32 <k.izumi721@gmail.com>
2021-02-14 18:01:20 +0900

    seed編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,10 +4,15 @@ User.create!(name:  "Example User",
              password_confirmation: "foobar",
              admin: true)
 
+User.create!(name:  "サンプルユーザー",
+             email: "user@sample.com",
+             password:              "foobar",
+             password_confirmation: "foobar")
+
 # 追加のユーザーをまとめて生成する
-99.times do |n|
+98.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+2}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
                email: email,
@@ -17,6 +22,7 @@ end
 
 user1 = User.first
 user2 = User.second
+user3 = User.third
 
 user1.microposts.create!(name: "極楽湯",
                         address: "埼玉県和光市",
@@ -30,15 +36,28 @@ user1.microposts.create!(name: "なごみの湯",
                         sauna: 1,
                         evaluate: 3)
 
-user2.microposts.create!(name: "久松湯",
+user2.microposts.create!(name: "極楽湯",
+                        address: "埼玉県和光市",
+                        price: 730,
+                        sauna: 1,
+                        evaluate: 4)
+
+user2.microposts.create!(name: "なごみの湯",
+                        address: "東京都杉並区",
+                        price: 2000,
+                        sauna: 1,
+                        evaluate: 3)
+
+user3.microposts.create!(name: "久松湯",
                         address: "東京都練馬区",
                         price: 900,
                         sauna: 1,
                         evaluate: 4)
 
 users = User.all
-user = User.first
 following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+followers = users[2..40]
+following.each { |followed| user1.follow(followed) }
+followers.each { |follower| follower.follow(user1) }
+following.each { |followed| user2.follow(followed) }
+followers.each { |follower| follower.follow(user2) }
