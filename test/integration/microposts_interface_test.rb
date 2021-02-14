ca55a2e80cd19da024c52d7ceaefbee8d8b61b06
izumi32 <k.izumi721@gmail.com>
2021-02-14 16:21:26 +0900

    検索フォームのテスト

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -4,6 +4,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+    @micropost = microposts(:sakae)
   end
 
   test "micropostのUI" do
@@ -19,7 +20,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
       post microposts_path, params: { micropost: { name: "かるまる",
                                                    address: "東京都豊島区",
                                                    price: 3000,
-                                                   sauna: 1,
+                                                   sauna: :presence,
                                                    evaluate: 4 } }
     end
     assert_redirected_to root_url
@@ -33,5 +34,36 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # 違うユーザーのプロフィールにアクセス（削除リンクがないことを確認）
     get user_path(users(:archer))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "micropostの検索" do
+    log_in @user
+    get root_url
+    # 有効な検索
+    get root_path, params: { search: { name: @micropost.name } }
+    assert_select "span", "名前：#{@micropost.name}"
+    get root_path, params: { search: { address: @micropost.address } }
+    assert_select "span", "住所：#{@micropost.address}"
+    get root_path, params: { search: { price_from: @micropost.price - 100,
+                                       price_to: @micropost.price + 100} }
+    assert_select "span", "料金：#{@micropost.price}円"
+    get root_path, params: { search: { sauna: @micropost.sauna } }
+    assert_select "span", "サウナ：#{Micropost.human_attribute_name "sauna.#{@micropost.sauna}"}"
+    get root_path, params: { search: { evaluate: @micropost.evaluate } }
+    assert_select "span", "評価：#{@micropost.evaluate}"
+    # 空で検索
+    get root_path, params: { search: { name: "",
+                                       address: "",
+                                       price_from: "",
+                                       price_to: "",
+                                       sauna: "",
+                                       evaluate: "" } }
+    @user.feed.each do |micropost|
+      assert_match micropost.name, response.body
+      assert_match micropost.address, response.body
+      assert_match micropost.price.to_s, response.body
+      assert_match Micropost.human_attribute_name("sauna.#{micropost.sauna}"), response.body
+      assert_match micropost.evaluate.to_s, response.body
+    end
   end
 end
--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -7,7 +7,7 @@ class MicropostTest < ActiveSupport::TestCase
     @micropost = @user.microposts.build(name: "極楽湯",
                                address: "埼玉県和光市",
                                price: 730,
-                               sauna: 1,
+                               sauna: :presence,
                                evaluate: 4,
                                user_id: @user.id)
   end
@@ -18,20 +18,6 @@ class MicropostTest < ActiveSupport::TestCase
 
   test "user_idがない場合" do
     @micropost.user_id = nil
-    assert_not @micropost.valid?
-  end
-
-  test "saunaが適正値以外の場合" do
-    @micropost.sauna = 2
-    assert_not @micropost.valid?
-    @micropost.sauna = 0.5
-    assert_not @micropost.valid?
-  end
-
-  test "evaluateが適正値以外の場合" do
-    @micropost.evaluate = 6
-    assert_not @micropost.valid?
-    @micropost.evaluate = 2.5
     assert_not @micropost.valid?
   end
 
